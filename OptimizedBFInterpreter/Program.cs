87e8618016d89786d3afce2a6249f7a825d731fa
class PythonAddict <classpythonaddike@gmail.com>
2021-03-06 11:00:05 +0530

    Optimization 1: Compile with release mode

--- OptimizedBFInterpreter/Program.cs
+++ OptimizedBFInterpreter/Program.cs
@@ -1,21 +1,127 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.IO;
 using System.Text;
-using System.Threading.Tasks;
 
-namespace OptimizedBFInterpreter
+namespace BFInterpreter
 {
 	class Program
 	{
+		public static int pointer = 0;
+		public static List<int> memory_blocks = new List<int>();
+		public static string BFCode;
+
 		static void Main(string[] args)
 		{
-			// The code provided will print ‘Hello World’ to the console.
-			// Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-			Console.WriteLine("Hello World!");
-			Console.ReadKey();
+			//Start the timer
+			DateTime start = DateTime.Now;
 
-			// Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+			// Interpret the file
+			using (FileStream fs = File.Open(args[0], FileMode.Open))
+			{
+				BFCode = "";
+
+				byte[] b = new byte[1024];
+				UTF8Encoding decoder = new UTF8Encoding(true);
+			
+				while (fs.Read(b, 0, b.Length) > 0)
+				{
+					BFCode += decoder.GetString(b);
+				}
+			}
+
+			memory_blocks.Add(0);
+		
+			try
+			{
+				ParseBF(BFCode);
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine("\nError:");
+				Console.WriteLine(e.Message);
+			}
+
+			DateTime end = DateTime.Now;
+
+			Console.WriteLine($"Time taken: {end.Subtract(start)}");
+
+			Console.Write("\nPress any key to exit . . . .");
+			Console.ReadKey();
+		}
+
+		static void ParseBF(string code)
+		{
+			int CurrentChar = 0, NumLoops, CharCopy;
+			char chr;
+
+			while (CurrentChar != code.Length)
+			{
+				if (code[CurrentChar] == '+')
+				{
+					memory_blocks[pointer] += 1;
+				}
+				else if (code[CurrentChar] == '-')
+				{
+					memory_blocks[pointer] -= 1;
+				}
+				else if (code[CurrentChar] == '>')
+				{
+					pointer += 1;
+				}
+				else if (code[CurrentChar] == '<')
+				{
+					pointer -= 1;
+				}
+				else if (code[CurrentChar] == '.')
+				{
+					Console.Write((char)memory_blocks[pointer]);
+				}
+				else if (code[CurrentChar] == ',')
+				{
+					chr = Console.ReadKey().ToString().ToCharArray()[0];
+					memory_blocks[pointer] = (int)chr;
+				}
+				else if (code[CurrentChar] == '/')
+				{
+					CurrentChar++;
+					while (code[CurrentChar] != '/')
+					{
+						CurrentChar++;
+					}
+				}
+				else if (code[CurrentChar] == '[')
+				{
+					NumLoops = 1;
+					CharCopy = CurrentChar + 1;
+
+					while (NumLoops != 0)
+					{
+						CurrentChar += 1;
+						if (code[CurrentChar] == '[')
+						{
+							NumLoops += 1;
+						}
+						else if (code[CurrentChar] == ']')
+						{
+							NumLoops -= 1;
+						}
+					}
+
+					while (memory_blocks[pointer] != 0)
+					{
+						ParseBF(code.Substring(CharCopy, CurrentChar - CharCopy));
+					}
+
+
+				}
+
+				CurrentChar += 1;
+				while (memory_blocks.Count <= pointer)
+				{
+					memory_blocks.Add(0);
+				}
+			}
 		}
 	}
 }
